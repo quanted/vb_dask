1ffd6b126a0de3afd70a9511e0b8dc1980a4b38e
dbsmith88 <dbsmith@uga.edu>
2020-09-01 10:20:11 -0500

    updated dask env

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ RUN apt update -y && \
     apt install git build-essential python3-dev libpq-dev -y && \
     pip install -U pip
 
+RUN pip list
+
 RUN cd /tmp && \
     git clone https://github.com/kurtw555/vb_django.git && \
     pip install --ignore-installed -r vb_django/requirements.txt  && \
--- dask_dev.sh
+++ dask_dev.sh
@@ -6,4 +6,4 @@ echo Enter your machine IP address:
 read IP
 docker rm -f dask-scheduler dask-worker
 docker run -d --name dask-scheduler -p 8786:8786 -p 8787:8787 vb_dask:0.2 dask-scheduler
-docker run -d --name dask-worker -v $VPATH:/app vb_dask:0.2 dask-worker tcp://$IP:8786
+docker run -d --name dask-worker --env DASK_SCHEDULER=tcp://$IP:8786 -v $VPATH:/app vb_dask:0.2 dask-worker --nprocs 3 --nthreads 2 --memory-limit 2GB tcp://$IP:8786
