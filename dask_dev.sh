48ba38964ac0536273ea70ce37f49f7fb3526174
dbsmith88 <dbsmith@uga.edu>
2020-09-02 15:24:07 -0500

    allow for env use for setup inputs

--- dask_dev.sh
+++ dask_dev.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 docker build -t vb_dask:0.2 .
-echo Enter absolute path to vb_django repo:
-read VPATH
-echo Enter your machine IP address:
-read IP
+echo "Use existing envs VB_DJANGO_PATH and VB_IP (y/n)?"
+read EXISTING
+if [ $EXISTING == "n" ]
+then
+  echo Enter absolute path to vb_django repo:
+  read VPATH
+  echo Enter your machine IP address:
+  read IP
+  setx VB_DJANGO_PATH "$VPATH"
+  setx VB_IP $IP
+else
+  IP=$VB_IP
+  echo Current IP: $VB_IP
+  VPATH=$VB_DJANGO_PATH
+  echo Current absolute path to vb_django directory: $VB_DJANGO_PATH
+fi
 docker rm -f dask-scheduler dask-worker
 docker run -d --name dask-scheduler -p 8786:8786 -p 8787:8787 vb_dask:0.2 dask-scheduler
-docker run -d --name dask-worker --env DASK_SCHEDULER=tcp://$IP:8786 -v $VPATH:/app vb_dask:0.2 dask-worker --nprocs 3 --nthreads 2 --memory-limit 2GB tcp://$IP:8786
+docker run -d --name dask-worker --env DASK_SCHEDULER=tcp://$IP:8786 -v "$VPATH":/app vb_dask:0.2 dask-worker --nprocs 3 --nthreads 2 --memory-limit 2GB tcp://$IP:8786
